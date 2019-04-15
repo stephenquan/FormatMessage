43459e0d7384924bb0453f608306c393c88cfe27
Stephen Quan <squan@esri.com>
2019-04-15 13:34:43 +1000

    Include multi lingual unit tests

--- FormatMessageTest.cpp
+++ FormatMessageTest.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "FormatMessage.h"
 
 #ifdef UNICODE
@@ -7,27 +7,24 @@
 #define COUT std::cout
 #endif
 
+void test(LPCTSTR testMessage, DWORD dwError, DWORD dwLanguageId = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT))
+{
+    OutputDebugString(testMessage);
+    OutputDebugString((LPTSTR)CFormatMessage(dwError, dwLanguageId));
+    OutputDebugString(_T("\n"));
+}
+
 int main()
 {
-    COUT << _T("ERROR_SUCCESS: ") << (LPTSTR) CFormatMessage(ERROR_SUCCESS) << _T("\n");
-    COUT << _T("ERROR_INVALID_FUNCTION: ") << (LPTSTR)CFormatMessage(ERROR_INVALID_FUNCTION) << _T("\n");
-    COUT << _T("ERROR_FILE_NOT_FOUND: ") << (LPTSTR)CFormatMessage(ERROR_FILE_NOT_FOUND) << _T("\n");
-    COUT << _T("ERROR_ACCESS_DENIED: ") << (LPTSTR)CFormatMessage(ERROR_ACCESS_DENIED) << _T("\n");
-    COUT << _T("ERROR_INVALID_HANDLE: ") << (LPTSTR)CFormatMessage(ERROR_INVALID_HANDLE) << _T("\n");
-    COUT << _T("ERROR_NOT_ENOUGH_MEMORY: ") << (LPTSTR)CFormatMessage(ERROR_NOT_ENOUGH_MEMORY) << _T("\n");
-    COUT << _T("ERROR_OUTOFMEMORY: ") << (LPTSTR)CFormatMessage(ERROR_OUTOFMEMORY) << _T("\n");
-    COUT << _T("ERROR_NOT_SUPPORTED: ") << (LPTSTR)CFormatMessage(ERROR_NOT_SUPPORTED) << _T("\n");
-
-    // Outputs:
-    //
-    // ERROR_SUCCESS: The operation completed successfully.
-    // ERROR_INVALID_FUNCTION : Incorrect function.
-    // ERROR_FILE_NOT_FOUND : The system cannot find the file specified.
-    // ERROR_ACCESS_DENIED : Access is denied.
-    // ERROR_INVALID_HANDLE : The handle is invalid.
-    // ERROR_NOT_ENOUGH_MEMORY : Not enough memory resources are available to process this command.
-    // ERROR_OUTOFMEMORY : Not enough memory resources are available to complete this operation.
-    // ERROR_NOT_SUPPORTED : The request is not supported.
-
+    test(_T("ERROR_SUCCESS (Default): "), ERROR_SUCCESS); // The operation completed successfully.
+    test(_T("ERROR_SUCCESS (French Française): "), ERROR_SUCCESS, MAKELANGID(LANG_FRENCH, SUBLANG_DEFAULT)); // L’opération a réussi.
+    test(_T("ERROR_SUCCESS (Japanese 日本語): "), ERROR_SUCCESS, MAKELANGID(LANG_JAPANESE, SUBLANG_DEFAULT)); // この操作を正しく終了しました。
+    test(_T("ERROR_INVALID_FUNCTION: "), ERROR_INVALID_FUNCTION); // Incorrect function.
+    test(_T("ERROR_FILE_NOT_FOUND: "), ERROR_FILE_NOT_FOUND); // The system cannot find the file specified.
+    test(_T("ERROR_ACCESS_DENIED: "), ERROR_ACCESS_DENIED); // Access is denied.
+    test(_T("ERROR_INVALID_HANDLE: "), ERROR_INVALID_HANDLE); // The handle is invalid.
+    test(_T("ERROR_NOT_ENOUGH_MEMORY: "), ERROR_NOT_ENOUGH_MEMORY); // Not enough memory resources are available to process this command.
+    test(_T("ERROR_OUTOFMEMORY: "), ERROR_OUTOFMEMORY); // Not enough memory resources are available to complete this operation.
+    test(_T("ERROR_NOT_SUPPORTED: "), ERROR_NOT_SUPPORTED); // The request is not supported.
     return 0;
 }
